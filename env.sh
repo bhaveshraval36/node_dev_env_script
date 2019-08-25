e034f56ccf8f5786f81ab09b78b497b646e7e0b0
bhavesh <you@example.com>
2019-08-25 19:32:00 +0530

    git added & nvm script file updated

--- env.sh
+++ env.sh
@@ -5,11 +5,16 @@ select_environment() {
     echo which dependency you want to install?
     select dependency_select in \
         "nvm" \
+        "git" \
         ; do
         case ${dependency_select} in
             'nvm' )
                 Dependency_ENV="nvm"
                 Dependency="nvmScript"
+                break;;
+            'git' )
+                Dependency_ENV="git"
+                Dependency="gitScript"
                 break;;
         esac
     done
@@ -21,6 +26,7 @@ function installing() {
     select_environment
     echo "installing ${Dependency_ENV} in your system .... "
     sh ./${Dependency}.sh 
+    echo "=> Plaese Close and reopen your terminal"
 }
 
 
